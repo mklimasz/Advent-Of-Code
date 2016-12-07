98a1f86cc61568708a7b12c64d87141bfccfd390
Mateusz Klimaszewski <mk.klimaszewski@gmail.com>
2016-12-07 21:17:52 +0100

    Day 6 - scala.

---
+++ day6/src/Puzzle1.scala
@@ -0,0 +1,26 @@
+object Puzzle1 {
+
+  def getListOfMaps(size: Int) : List[Map[Char, Int]] = {
+    List.fill(size)(Map[Char, Int]() withDefaultValue 0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    io.Source.fromFile("./input.txt")
+      .getLines()
+      .foldLeft(getListOfMaps(8)) {
+        (maps, line) => {
+          (maps, line).zipped.map {
+            (map, c) => {
+              map + (c -> (1 + map(c)))
+            }
+          }
+        }
+      }
+      .foreach(map => {
+        print(map.toList
+          .sortBy {case (k,v) => (-v, k)}
+          .head
+          ._1)
+      })
+  }
+}
